d503cf73da1fc063025b85b2dcdb64d4c6a662eb
arunreddy-padala <padala.a@northeastern.edu>
2022-05-22 17:58:24 -0400

    Database and repositories setup

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source
+
+import androidx.room.*
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM note")
+    fun getNotes(): Flow<List<Note>>
+
+    @Query("SELECT * from note where id= :id")
+    suspend fun getNoTeByID(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+
+
+}
