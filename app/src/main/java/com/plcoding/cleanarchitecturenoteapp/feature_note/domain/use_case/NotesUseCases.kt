421ef55027e4326b32a717330274e3e65aff5268
arunreddy-padala <padala.a@northeastern.edu>
2022-05-22 18:37:07 -0400

    Get Notes and Delete Notes use cases

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NotesUseCases.kt
@@ -0,0 +1,9 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+data class NotesUseCases(
+
+    val getNotes: GetNotesUseCase,
+    val deleteNote: DeleteNoteUseCase
+
+
+)
---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/util/OrderType.kt
@@ -0,0 +1,8 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.util
+
+sealed class OrderType {
+
+    object Ascending : OrderType()
+    object Descending : OrderType()
+
+}
