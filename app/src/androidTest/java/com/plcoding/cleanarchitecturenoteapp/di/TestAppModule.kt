243cdc3ef191fe6074bcc131c3e5cac41f4e39d3
arunreddy-padala <padala.a@northeastern.edu>
2022-06-13 21:20:05 -0400

    Instrumented tests to test toggle order selection

---
+++ app/src/androidTest/java/com/plcoding/cleanarchitecturenoteapp/di/TestAppModule.kt
@@ -0,0 +1,65 @@
+package com.plcoding.cleanarchitecturenoteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDatabase
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+
+        return Room.inMemoryDatabaseBuilder(
+
+            app,
+            NoteDatabase::class.java,).build()
+
+
+    @Provides
+    @Singleton
+            fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+
+                return NoteRepositoryImpl(db.noteDao)
+
+
+            }
+
+
+        @Provides
+        @Singleton
+                fun provideNoteUseCases(repository: NoteRepository): NotesUseCases {
+
+                    return NotesUseCases(
+
+                        getNotes = GetNotesUseCase(repository),
+                        deleteNote = DeleteNoteUseCase(repository),
+                        addNote = AddNotesUseCase(repository),
+                        getNote = GetNoteUseCase(repository)
+
+                    )
+
+
+                }
+
+
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
@@ -14,9 +14,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.plcoding.cleanarchitecturenoteapp.core.util.TestTags
 import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.components.NoteItem
 import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.components.OrderSection
 import kotlinx.coroutines.launch
@@ -36,11 +38,10 @@ fun NotesScreen(
             FloatingActionButton(
                 onClick = {
                     navController.navigate(com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.util.Screen.AddEditNoteScreen.route)
-
                 },
                 backgroundColor = MaterialTheme.colors.primary
             ) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Note")
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
             }
         },
         scaffoldState = scaffoldState
@@ -56,7 +57,7 @@ fun NotesScreen(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = "Your Note",
+                    text = "Your note",
                     style = MaterialTheme.typography.h4
                 )
                 IconButton(
@@ -78,7 +79,8 @@ fun NotesScreen(
                 OrderSection(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 16.dp),
+                        .padding(vertical = 16.dp)
+                        .testTag(TestTags.ORDER_SECTION),
                     noteOrder = state.noteOrder,
                     onOrderChange = {
                         viewModel.onEvent(NotesEvent.Order(it))
@@ -97,13 +99,12 @@ fun NotesScreen(
                                     com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.util.Screen.AddEditNoteScreen.route +
                                             "?noteId=${note.id}&noteColor=${note.color}"
                                 )
-
                             },
                         onDeleteClick = {
                             viewModel.onEvent(NotesEvent.DeleteNote(note))
                             scope.launch {
                                 val result = scaffoldState.snackbarHostState.showSnackbar(
-                                    message = "Note Deleted",
+                                    message = "Note deleted",
                                     actionLabel = "Undo"
                                 )
                                 if(result == SnackbarResult.ActionPerformed) {
